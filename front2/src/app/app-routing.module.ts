2eed15d5e403932be6bd73d16ae0debd85605f46
Jeff Cogswell <jeffcogs@gmail.com>
2022-08-26 17:55:33 -0400

    Init

---
+++ front2/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'test/page1', component: Page1Component },
+  { path: 'test/page2', component: Page2Component }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
